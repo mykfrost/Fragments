4061eb6c3f73a0ce1816a87bb53e3b2fe897a1c3
MichaelOtieno <okellofrost@gmail.com>
2024-04-28 10:25:32 +0300

    creating flower class

---
+++ app/src/main/java/com/example/android/fragaments/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.android.fragaments;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.android.fragaments.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/android/fragaments/ui/main/Flower.java
@@ -0,0 +1,68 @@
+package com.example.android.fragaments.ui.main;
+
+import android.os.Bundle;
+
+public class Flower {
+    public static final String FLOWER_NAME = "flower_Name";
+    public static final String IMAGE_RESOURCE = "image_Resource";
+
+    public static final String PRICE = "price";
+    public static final String INSTRUCTIONS = "instructions";
+
+    public String getFlowerName() {
+        return flowerName;
+    }
+
+    public void setFlowerName(String flowerName) {
+        this.flowerName = flowerName;
+    }
+
+    public int getImageResource() {
+        return imageResource;
+    }
+
+    public void setImageResource(int imageResource) {
+        this.imageResource = imageResource;
+    }
+
+    public String getInstructions() {
+        return instructions;
+    }
+
+    public void setInstructions(String instructions) {
+        this.instructions = instructions;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+
+
+    public Flower(String flowerName, int imageResource, String instructions, double price) {
+        this.flowerName = flowerName;
+        this.imageResource = imageResource;
+        this.instructions = instructions;
+        this.price = price;
+    }
+
+    // create from a bundle
+    public Flower(Bundle bundle){
+        if (bundle != null) {
+            this.flowerName =  bundle.getString(FLOWER_NAME);
+            this.imageResource = bundle.getInt(IMAGE_RESOURCE);
+            this.price = bundle.getDouble(PRICE);
+            this.instructions = bundle.getString(INSTRUCTIONS);
+        }
+    }
+    private String flowerName;
+    private int imageResource;
+    private String instructions;
+    private  double price;
+
+
+}
